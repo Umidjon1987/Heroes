80733fdd1b145823c9247b1133f98aeaccd2416a
Umidjon1987 <92720735+Umidjon1987@users.noreply.github.com>
2022-08-30 15:59:08 +0600

    30_08_2022

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import store from "../../store"
 
 import { selectAll } from '../heroesFilters/filtersSlice';
-import { heroCreated } from '../../components/heroesList/heroesSlice';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 
 const HeroesAddForm = () => {
     // Состояния для контроля формы
@@ -13,10 +13,10 @@ const HeroesAddForm = () => {
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
+    const [createHero, {isLoading}] = useCreateHeroMutation();
+
     const {filtersLoadingStatus} = useSelector(state => state.filters);
     const filters = selectAll(store.getState());
-    const dispatch = useDispatch();
-    const {request} = useHttp();
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -30,12 +30,7 @@ const HeroesAddForm = () => {
             element: heroElement
         }
 
-        // Отправляем данные на сервер в формате JSON
-        // ТОЛЬКО если запрос успешен - отправляем персонажа в store
-        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(res => console.log(res, 'Отправка успешна'))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
+        createHero(newHero).unwrap();
 
         // Очищаем форму после отправки
         setHeroName('');
